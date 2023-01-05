bc0695fbefe1fc5ff4bb3f9879c9a61fba4b23e0
abraham perez reyes <abrahampre27@gmail.com>
2023-01-04 18:21:05 -0600

    first list and dev added to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,26 @@ var taskToDoEl =  document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event){
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+    
+    // create list Element
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
-    taskToDoEl.appendChild(listItemEl)
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML to the div 
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3> <span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    taskToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl);
 }
 
 formEl.addEventListener("submit", createTaskHandler)
